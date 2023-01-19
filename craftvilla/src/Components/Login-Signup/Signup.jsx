06ab817246dda703a11d8fb685faec0f78d080d7
RajatMujawar123git add.git commit -m evaluationgit config --global user.email mujawarrajat009@gmail.comgit config --global user.name RajatMujawar123 <mujawarrajat009@gmail.com>
2023-01-19 22:37:48 +0530

    day3

---
+++ craftvilla/src/Components/Login-Signup/Signup.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <div>
+      <form >
+        <input type="text" placeholder='Email'/>
+        <input type="text" placeholder='Mobile No.' />
+        <input type="text" placeholder='Password'/>
+        <input type="submit" />
+      </form>
+    </div>
+  )
+}
+
+export default Signup
