a57676d123296fde66b18977a9b6d170c5a996a1
Deepak-197 <soni5615@gmail.com>
2023-01-19 23:28:52 +0530

    conflict

--- craftvilla/src/Components/Header/Header.jsx
+++ craftvilla/src/Components/Header/Header.jsx
@@ -5,7 +5,9 @@ export const Header = () => {
   return (
     <div className='Header'>
      <div className='Header-child'>
+
       <div><h2>Home Decor</h2></div>
+      
       <div><h2>Products Near me</h2></div>
     </div>
     </div>
