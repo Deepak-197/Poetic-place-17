6b55cb4b5330693ee7dff3c65980de2a3997f192
Deepak-197 <soni5615@gmail.com>
2023-01-19 23:10:22 +0530

    day-3 completed

--- craftvilla/src/Components/Header/Header.jsx
+++ craftvilla/src/Components/Header/Header.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import './Header.css';
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <div className='Header'>
+     <div className='Header-child'>
+      <div><h2>Home Decor</h2></div>
+      <div><h2>Products Near me</h2></div>
+    </div>
+    </div>
   )
 }
--- craftvilla/src/Components/Navbar/Navbar.jsx
+++ craftvilla/src/Components/Navbar/Navbar.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
+import React from 'react';
+import "./Navbar.css";
 
 export const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='Navbar'>
+      <div><img className='Navbar-Logo' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRA7OH2_8xVgMEca8J42A5DkebhjcQR3RJNfA&usqp=CAU' alt='' /></div>
+      <div className='search-bar'>
+       <span>SEARCH </span>
+       <input className='input-bar'  placeholder='|  Type your location here' />
+       <span>🔍</span>
+      </div>
+      <div>
+       <label>Sign In / Register</label>
+      </div>
+    </div>
   )
 }
