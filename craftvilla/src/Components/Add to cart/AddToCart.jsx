b21f7bedf3636d3bb57b79fc1fa397112554c240
princepatel7405 <prince.m.7405@gmail.com>
2023-01-20 23:24:40 +0530

    Dat-4 work done

--- craftvilla/src/Components/Add to cart/AddToCart.jsx
+++ craftvilla/src/Components/Add to cart/AddToCart.jsx
@@ -5,15 +5,32 @@ import SingleCart from "../singleCart/SingleCart";
 
 const AddToCart = () => {
   const [data, setData] = useState([]);
+  const [total, setTotal] = useState(0);
+
   const getData = () => {
     axios
       .get("http://localhost:8080/cart")
       .then((res) => setData(res.data))
       .catch((err) => console.log(err));
   };
-  const handleRemoval=(id)=>{
-    axios.delete(`http://localhost:8080/cart/${id}`).then(()=>getData()).then(res=>setData(res.data))
-}
+  const handleRemoval = (id) => {
+    axios
+      .delete(`http://localhost:8080/cart/${id}`)
+      .then(() => getData())
+      .then((res) => setData(res.data));
+  };
+
+  let totalPrice = data.reduce((initial, e) => {
+    let { price, count } = e;
+
+    price = Number(price.trim().split(" ")[1]);
+
+    initial = initial + price * count;
+
+    return initial;
+  }, 0);
+  
+  
   useEffect(() => {
     getData();
   }, []);
@@ -25,7 +42,7 @@ const AddToCart = () => {
           <h3>My Cart({data.length} items)</h3>
         </div>
         <div className="right-bar">
-          <h3>SubTotal:410</h3>
+          <h3>SubTotal:₹{totalPrice+50}</h3>
           <div>
             <button className="place-order-button">Place Order</button>
           </div>
@@ -39,11 +56,32 @@ const AddToCart = () => {
         <h3>Price </h3>
       </div>
       <hr color="#cccccc" />
-      {data?.map(e=>{
+      {data?.map((e) => {
         return (
-          <SingleCart key={e.id} {...e} handleRemoval={()=>handleRemoval(e.id)}/>
-        )
+          <>
+            <SingleCart
+              key={e.id}
+              {...e}
+              handleRemoval={() => handleRemoval(e.id)}
+              getData={getData}
+            />
+          </>
+        );
       })}
+
+      <div className="bottom-details">
+        <div className="address-details">
+        <h3>Deliver To: </h3> <br/> 
+        <h3> Prince Patel </h3>
+        Bhavnagar,Gujarat
+        </div>
+        <div>
+          <h3>Price Details</h3> <br />
+          Price  ₹{totalPrice} <br />
+          Shipping ₹50 <br />
+          subTotal  ₹{totalPrice+50} <br />
+        </div>
+      </div>
     </div>
   );
 };
