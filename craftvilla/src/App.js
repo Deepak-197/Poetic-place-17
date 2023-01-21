cb003fb9253c9aa7e86011da45c241e46ff8ea3c
Deepak-197 <soni5615@gmail.com>
2023-01-21 13:25:43 +0530

    Day-4 completed

--- craftvilla/src/App.js
+++ craftvilla/src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import { Footer } from './Components/Footer';
 import { Header } from './Components/Header/Header';
 import { Navbar } from './Components/Navbar/Navbar';
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
     <Navbar />
     <Header />
+    <Footer />
     </div>
   );
 }
