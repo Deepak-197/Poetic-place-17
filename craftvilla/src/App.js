650eaac747be7794877772a872c808aa43286282
Rii-C <rrishi535@gmail.com>
2023-01-22 20:45:38 +0530

    last_commit

--- craftvilla/src/App.js
+++ craftvilla/src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { AddProduct } from './Components/Admin/AddProduct/AddProduct';
+import { Dashboard } from './Components/Admin/DashBoard/Dashboard';
+import { AdminRoute } from './Components/Admin/AdminRoute/AdminRouts';
+import { AdminPageRoute } from './Components/AdminPageRoute';
+import { Order } from './Components/Admin/Order/Order';
 
 function App() {
   return (
     <div className="App">
-      
+      <> 
+      {/* <Order/> */}
+      <AdminPageRoute/> 
+      {/* <AdminRoute/> */}
+      {/* <AddProduct/> */}
+      {/* <Dashboard/> */}
+      </>
     </div>
   );
 }
